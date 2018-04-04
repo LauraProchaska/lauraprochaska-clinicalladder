88bf29079aa44c025b97eb68c3cbfdd0db13e472
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-04-03 23:16:22 -0500

    save changes to person site

---
+++ LP-function.js
@@ -0,0 +1,17 @@
+
+//
+// document.getElementById("button1").onclick = function() {
+//     window.location.href ="https://www.github.com/lalepro", target="_blank";
+// };
+//
+// document.getElementById("button2").onclick = function() {
+//     window.location.href ="https://www.linkedin.com/in/laura-prochaska/"; target="_blank";
+// };
+//
+// document.getElementById("button3").onclick = function() {
+//     window.location.href ="https://www.instagram.com/lalepro/", target="_blank";
+// };
+//
+// document.getElementById("button4").onclick = function() {
+//     window.location.href ="https://www.facebook.com/proartphotos/", target="_blank";
+// };
